7cda887c83ad85768dc559989cc1e03d804be74b
maria <guermoud.maria@gmail.com>
2025-01-14 14:43:59 +0100

    first commit

---
+++ Eureka/FiegnClient/Voiture/src/main/java/exemple/feignclient/EurikaFeignClientApplication.java
@@ -0,0 +1,30 @@
+package exemple.feignclient;
+import exemple.feignclient.entities.Voiture;
+import exemple.feignclient.repository.VoitureRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@EnableFeignClients
+@SpringBootApplication
+public class EurikaFeignClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurikaFeignClientApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner initDatabase(VoitureRepository voitureRepository) {
+        return args -> {
+            Voiture voiture1 = new Voiture(null, "Toyota", "ABC123", "Corolla", 1L);
+            Voiture voiture2 = new Voiture(null, "Honda", "XYZ456", "Civic", 2L);
+            Voiture voiture3 = new Voiture(null, "Ford", "LMN789", "Focus", 1L);
+            Voiture voiture4 = new Voiture(null, "Chevrolet", "DEF012", "Impala", 3L);
+
+            voitureRepository.saveAll(Arrays.asList(voiture1, voiture2, voiture3, voiture4));
+        };
+    }
+}
---
+++ Eureka/FiegnClient/Voiture/src/test/java/exemple/eurekafeignclient/EurekaFeignClientApplicationTests.java
@@ -0,0 +1,15 @@
+package exemple.eurekafeignclient;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+/*
+@SpringBootTest(classes = ConsulFeignClientApplication.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
+class EurekaFeignClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Test si le contexte Spring Boot se charge correctement
+    }
+}
+*/
