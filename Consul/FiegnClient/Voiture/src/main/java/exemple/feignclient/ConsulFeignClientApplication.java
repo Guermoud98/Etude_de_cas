7237ba4a69f0a1a4e48f252f19edef9b55512655
Guermoud98 <57940550+Guermoud98@users.noreply.github.com>
2025-01-14 14:51:57 +0100

    Update ConsulFeignClientApplication.java

--- Consul/FiegnClient/Voiture/src/main/java/exemple/feignclient/ConsulFeignClientApplication.java
+++ Consul/FiegnClient/Voiture/src/main/java/exemple/feignclient/ConsulFeignClientApplication.java
@@ -19,15 +19,4 @@ public class ConsulFeignClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsulFeignClientApplication.class, args);
     }
-    /*@Bean
-    CommandLineRunner initDatabase(VoitureRepository voitureRepository) {
-        return args -> {
-            Voiture voiture1 = new Voiture(null, "Toyota", "ABC123", "Corolla", 1L);
-            Voiture voiture2 = new Voiture(null, "Honda", "XYZ456", "Civic", 2L);
-            Voiture voiture3 = new Voiture(null, "Ford", "LMN789", "Focus", 1L);
-            Voiture voiture4 = new Voiture(null, "Chevrolet", "DEF012", "Impala", 3L);
-
-            voitureRepository.saveAll(Arrays.asList(voiture1, voiture2, voiture3, voiture4));
-        };
-    }*/
 }
